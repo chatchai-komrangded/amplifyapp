81e365bb109815972754ff243cb4e7c8b4578656
Komrangded <chatkom@38f9d3715728.ant.amazon.com>
2021-04-28 21:58:02 +0700

    added auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+Amplify.configure(config);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
