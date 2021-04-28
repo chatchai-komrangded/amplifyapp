74b9a1f27e5b8780f9549184eaf7aaad9e81ecb2
Komrangded <chatkom@38f9d3715728.ant.amazon.com>
2021-04-28 22:48:35 +0700

    final version

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
       <h1>My Notes App</h1>
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
+        placeholder="Note names"
         value={formData.name}
       />
       <input
